46e5e356d8233bf3d8671378a70862e49b7caba4
Thamer Dridi <thamer.dridi@etudiant-isi.utm.tn>
2024-02-11 19:11:06 +0200

    Initial commit

---
+++ token/src/lib.rs
@@ -0,0 +1,54 @@
+use near_contract_standards::fungible_token::metadata::{
+    FungibleTokenMetadata, FungibleTokenMetadataProvider,
+};
+use near_contract_standards::fungible_token::FungibleToken;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::{ValidAccountId, U128};
+use near_sdk::{env, BorshStorageKey, near_bindgen, AccountId, PanicOnDefault, PromiseOrValue};
+
+near_sdk::setup_alloc!();
+
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
+pub struct Contract {
+    token: FungibleToken,
+    metadata: LazyOption<FungibleTokenMetadata>,
+}
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    Token,
+    Metadata,
+}
+
+#[near_bindgen]
+impl Contract {
+    /// Initializes the contract with the given total supply owned by the given `owner_id` with
+    /// the given fungible token metadata.
+    #[init]
+    pub fn new(
+        owner_id: ValidAccountId,
+        total_supply: U128,
+        metadata: FungibleTokenMetadata,
+    ) -> Self {
+        metadata.assert_valid();
+        let mut this = Self {
+            token: FungibleToken::new(StorageKey::Token),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+        };
+        this.token.internal_register_account(owner_id.as_ref());
+        this.token.internal_deposit(owner_id.as_ref(), total_supply.into());
+        this
+    }
+}
+
+near_contract_standards::impl_fungible_token_core!(Contract, token);
+near_contract_standards::impl_fungible_token_storage!(Contract, token);
+
+#[near_bindgen]
+impl FungibleTokenMetadataProvider for Contract {
+    fn ft_metadata(&self) -> FungibleTokenMetadata {
+        self.metadata.get().unwrap()
+    }
+}
